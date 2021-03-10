a67a6ba198e7fcf62fbf6aae5b7782ae37e1c353
Dell Du <18588220928@163.com>
2021-03-11 00:18:10 +0800

    Update.

---
+++ image_facegan/cmaes.h
@@ -0,0 +1,218 @@
+/* --------------------------------------------------------- */
+/* --- File: cmaes.h ----------- Author: Nikolaus Hansen --- */
+/* ---------------------- last modified: IX 2010         --- */
+/* --------------------------------- by: Nikolaus Hansen --- */
+/* --------------------------------------------------------- */
+/*   
+     CMA-ES for non-linear function minimization. 
+
+     Copyright (C) 1996, 2003-2010  Nikolaus Hansen. 
+     e-mail: nikolaus.hansen (you know what) inria.fr
+      
+     License: see file cmaes.c
+   
+*/
+#ifndef NH_cmaes_h				/* only include ones */
+#define NH_cmaes_h
+
+#include <time.h>
+
+#define CheckPoint(fmt, arg...) printf("# CheckPoint: %d(%s): " fmt "\n", (int)__LINE__, __FILE__, ##arg)
+
+typedef int(*SampleTransformer)(double *, int);
+
+typedef struct
+/* cmaes_random_t 
+ * sets up a pseudo random number generator instance 
+ */
+{
+	/* Variables for Uniform() */
+	long int startseed;
+	long int aktseed;
+	long int aktrand;
+
+	/* Variables for Gauss() */
+	short flgstored;
+	double hold;
+} cmaes_random_t;
+
+typedef struct
+/* cmaes_timings_t 
+ * time measurement, used to time eigendecomposition 
+ */
+{
+	/* for outside use */
+	double totaltime;			/* zeroed by calling re-calling cmaes_timings_start */
+	double totaltotaltime;
+	double tictoctime;
+	double lasttictoctime;
+
+	/* local fields */
+	clock_t lastclock;
+	time_t lasttime;
+	clock_t ticclock;
+	time_t tictime;
+	short istic;
+	short isstarted;
+
+	double lastdiff;
+	double tictoczwischensumme;
+} cmaes_timings_t;
+
+typedef struct
+/* cmaes_readpara_t
+ * collects all parameters, in particular those that are read from 
+ * a file before to start. This should split in future? 
+ */
+{
+	char *filename;				/* keep record of the file that was taken to read parameters */
+	short flgsupplemented;
+
+	/* input parameters */
+	int N;						/* problem dimension, must stay constant, should be unsigned or long? */
+	unsigned int seed;
+	double *xstart;
+	double *typicalX;
+	int typicalXcase;
+	double *rgInitialStds;
+	double *rgDiffMinChange;
+
+	/* termination parameters */
+	double stopMaxFunEvals;
+	double facmaxeval;
+	double stopMaxIter;
+	struct {
+		int flg;
+		double val;
+	} stopFitness;
+	double stopTolFun;
+	double stopTolFunHist;
+	double stopTolX;
+	double stopTolUpXFactor;
+
+	/* internal evolution strategy parameters */
+	int lambda;					/* -> mu, <- N */
+	int mu;						/* -> weights, (lambda) */
+	double mucov, mueff;		/* <- weights */
+	double *weights;			/* <- mu, -> mueff, mucov, ccov */
+	double damps;				/* <- cs, maxeval, lambda */
+	double cs;					/* -> damps, <- N */
+	double ccumcov;				/* <- N */
+	double ccov;				/* <- mucov, <- N */
+	double diagonalCov;			/* number of initial iterations */
+	struct {
+		int flgalways;
+		double modulo;
+		double maxtime;
+	} updateCmode;
+	double facupdateCmode;
+
+	/* supplementary variables */
+
+	char *weigkey;
+	char resumefile[99];
+	const char **rgsformat;
+	void **rgpadr;
+	const char **rgskeyar;
+	double ***rgp2adr;
+	int n1para, n1outpara;
+	int n2para;
+} cmaes_readpara_t;
+
+typedef struct
+/* cmaes_t 
+ * CMA-ES "object" 
+ */
+{
+	const char *version;
+	/* char *signalsFilename; */
+	cmaes_readpara_t sp;
+	cmaes_random_t rand;		/* random number generator */
+
+	double sigma;				/* step size */
+
+	double *rgxmean;			/* mean x vector, "parent" */
+	double *rgxbestever;
+	double **rgrgx;				/* range of x-vectors, lambda offspring */
+	int *index;					/* sorting index of sample pop. */
+	double *arFuncValueHist;
+
+	short flgIniphase;			/* not really in use anymore */
+	short flgStop;
+
+	double chiN;
+	double **C;					/* lower triangular matrix: i>=j for C[i][j] */
+	double **B;					/* matrix with normalize eigenvectors in columns */
+	double *rgD;				/* axis lengths */
+
+	double *rgpc;
+	double *rgps;
+	double *rgxold;
+	double *rgout;
+	double *rgBDz;				/* for B*D*z */
+	double *rgdTmp;				/* temporary (random) vector used in different places */
+	double *rgFuncValue;
+	double *publicFitness;		/* returned by cmaes_init() */
+
+	double gen;					/* Generation number */
+	double countevals;
+	double state;				/* 1 == sampled, 2 == not in use anymore, 3 == updated */
+
+	double maxdiagC;			/* repeatedly used for output */
+	double mindiagC;
+	double maxEW;
+	double minEW;
+
+	char sOutString[330];		/* 4x80 */
+
+	short flgEigensysIsUptodate;
+	short flgCheckEigen;		/* control via cmaes_signals.par */
+	double genOfEigensysUpdate;
+	cmaes_timings_t eigenTimings;
+
+	double dMaxSignifKond;
+	double dLastMinEWgroesserNull;
+
+	short flgresumedone;
+
+	time_t printtime;
+	time_t writetime;			/* ideally should keep track for each output file */
+	time_t firstwritetime;
+	time_t firstprinttime;
+
+} cmaes_t;
+
+/* --- initialization, constructors, destructors --- */
+
+	double *cmaes_init(cmaes_t *, int dimension, double *xstart,
+					   double *stddev, long seed, int lambda, const char *input_parameter_filename);
+	void cmaes_init_para(cmaes_t *, int dimension, double *xstart,
+						 double *stddev, long seed, int lambda, const char *input_parameter_filename);
+	double *cmaes_init_final(cmaes_t *);
+	void cmaes_resume_distribution(cmaes_t * evo_ptr, char *filename);
+	void cmaes_exit(cmaes_t *);
+
+/* --- core functions --- */
+	double *const *cmaes_SamplePopulation(cmaes_t *, SampleTransformer st);
+	double *cmaes_UpdateDistribution(cmaes_t *, const double *rgFitnessValues);
+	const char *cmaes_TestForTermination(cmaes_t *);
+
+/* --- additional functions --- */
+	double *const *cmaes_ReSampleSingle(cmaes_t * t, int index);
+	double *cmaes_SampleSingleInto(cmaes_t * t, double *rgx);
+	void cmaes_UpdateEigensystem(cmaes_t *, int flgforce);
+
+/* --- getter functions --- */
+	double cmaes_Get(cmaes_t *, char const *keyword);
+	const double *cmaes_GetPtr(cmaes_t *, char const *keyword);	/* e.g. "xbestever" */
+	double *cmaes_GetNew(cmaes_t * t, char const *keyword);	/* user is responsible to free */
+	double *cmaes_GetInto(cmaes_t * t, char const *keyword, double *mem);	/* allocs if mem==NULL, user is responsible to free */
+
+/* --- online control and output --- */
+	void cmaes_ReadSignals(cmaes_t *, char const *filename);
+	void cmaes_WriteToFile(cmaes_t *, const char *szKeyWord, const char *output_filename);
+	char *cmaes_SayHello(cmaes_t *);
+/* --- misc --- */
+	double *cmaes_NewDouble(int n);	/* user is responsible to free */
+	void cmaes_FATAL(char const *s1, char const *s2, char const *s3, char const *s4);
+#endif
--- image_facegan/main.c
+++ image_facegan/main.c
@@ -16,28 +16,201 @@
 #include <nimage/nnmsg.h>
 
 #include "engine.h"
+#include "cmaes.h"
 
-#define IMAGE_FACEGAN_REQCODE 0x0105
+#define IMAGE_FACEGAN_REQCODE 0x0108
 // #define IMAGE_FACEGAN_URL "ipc:///tmp/image_facegan.ipc"
-#define IMAGE_FACEGAN_URL "tcp://127.0.0.1:9105"
+#define IMAGE_FACEGAN_URL "tcp://127.0.0.1:9108"
+
+#define W_SPACE_DIM 512
+
+OrtEngine *decoder_engine = NULL;
+OrtEngine *transformer_engine = NULL;
+TENSOR *stand_tensor = NULL;
+
+// Transform zcode to wcode
+int sample_t(double *x, int n)
+{
+	int i;
+	float *f;
+	TENSOR *zcode_tensor, *wcode_tensor;
+
+	zcode_tensor = tensor_create(1, 1, 1, W_SPACE_DIM);
+
+	wcode_tensor = TensorForward(transformer_engine, zcode_tensor);
+
+	// Update x
+	f = wcode_tensor->data;
+	for (i = 0; i < n; i++)
+		*x++ = (double)*f++;
+
+	tensor_destroy(wcode_tensor);
+	tensor_destroy(zcode_tensor);
+
+	return 0;
+}
+
+double fitfun(double const *x, unsigned long N)
+{
+	int i;
+	double sum;
+	float *f1, *f2;
+	TENSOR *wcode_tensor, *output_image_tensor, *output_tensor;
+
+	// Create wcode tensor
+	wcode_tensor = tensor_create(1, 1, 1, W_SPACE_DIM);
+	f1 = wcode_tensor->data;
+	for (i = 0; i < W_SPACE_DIM; i++) {
+		*f1 = (float)(*x);
+		f1++; x++;
+	}
+
+	output_image_tensor = TensorForward(decoder_engine, wcode_tensor);
+	output_tensor = tensor_zoom(output_image_tensor, 256, 256);
+
+	// Compute loss
+	sum = 0;
+	f1 = stand_tensor->data;
+	f2 = output_tensor->data;
+	for (i = 0; i < (int)N; i++) {
+		sum += (double)(*f1 - *f2) * (*f1 - *f2);
+		f1++; f2++;
+	}
+
+	// Release tensors
+	tensor_destroy(output_image_tensor);
+	tensor_destroy(output_tensor);
+	tensor_destroy(wcode_tensor);
+
+	return sum;
+}
+
+double *cmaes_search(int epochs)
+{
+	int i, dimension;
+	cmaes_t evo;
+	double *cost_values, *xfinal, *const *pop;
+
+	// cost_values = cmaes_init(&evo, W_SPACE_DIM dimmesion, xstart, stddev, 0, / * lambda */, "none");
+	cmaes_init_para(&evo, W_SPACE_DIM /*dimmesion*/, NULL, NULL, 0, 0 /* lambda */, "none");
+
+	evo.sp.stopMaxIter = epochs;	// stop after given number of iterations (generations)
+	evo.sp.stopFitness.flg = 1;
+	evo.sp.stopFitness.val = 1e-3;	// stop if function value is smaller than stopFitness
+	evo.sp.stopTolFun = 1e-4;		// stop if function value differences are small
+	cost_values =  cmaes_init_final(&evo);
+
+
+	dimension = (unsigned int) cmaes_Get(&evo, "dimension");
+	printf("%s\n", cmaes_SayHello(&evo));
+
+	while (!cmaes_TestForTermination(&evo)) {
+		/* generate lambda new search points, sample population */
+		pop = cmaes_SamplePopulation(&evo, sample_t);	/* do not change content of pop */
+
+		for (i = 0; i < cmaes_Get(&evo, "lambda"); ++i) {
+			cost_values[i] = fitfun(pop[i], dimension);	/* evaluate */
+		}
+
+		cmaes_UpdateDistribution(&evo, cost_values);	/* assumes that pop[i] has not been modified */
+
+		if ((int)evo.gen % 10 == 0) {
+			printf("Progress %6.2f %% ...\n", (float)(100.0 * evo.gen/epochs));
+			fflush(stdout);
+		}
+	}
+	printf("Stop Condition:\n%s\n", cmaes_TestForTermination(&evo));	/* print termination reason */
+
+	cmaes_WriteToFile(&evo, "all", "/tmp/cmaes_results.txt");	/* write final results */
+
+	/* get best estimator for the optimum, xmean */
+	xfinal = cmaes_GetNew(&evo, "xmean");
+
+	cmaes_exit(&evo);			/* release memory */
+
+	return xfinal;
+}
+
+TENSOR *wcode_search(TENSOR *reference_tensor)
+{
+	CHECK_TENSOR(reference_tensor);
+
+	return NULL;
+}
+
+int FaceGanService(char *endpoint, int use_gpu)
+{
+	int socket, reqcode, count, rescode;
+	TENSOR *input_tensor, *output_tensor, *wcode_tensor;
+
+	if ((socket = server_open(endpoint)) < 0)
+		return RET_ERROR;
+
+	decoder_engine = CreateEngine("image_gandecoder.onnx", use_gpu);
+	CheckEngine(decoder_engine);
+
+	transformer_engine = CreateEngine("image_gantransformer.onnx", use_gpu);
+	CheckEngine(transformer_engine);
+
+
+	count = 0;
+	for (;;) {
+		syslog_info("Service %d times", count);
+
+		input_tensor = request_recv(socket, &reqcode);
+
+		if (!tensor_valid(input_tensor)) {
+			syslog_error("Request recv bad tensor ...");
+			continue;
+		}
+		syslog_info("Request Code = %d", reqcode);
+
+		// Real service ...
+		time_reset();
+		wcode_tensor = wcode_search(input_tensor); check_tensor(wcode_tensor);
+
+		output_tensor = TensorForward(decoder_engine, wcode_tensor);
+		time_spend((char *)"Infer");
+
+		if (tensor_valid(output_tensor)) {
+			rescode = reqcode;
+			response_send(socket, output_tensor, rescode);
+			tensor_destroy(output_tensor);
+		}
+
+		tensor_destroy(wcode_tensor);
+
+		tensor_destroy(input_tensor);
+
+		count++;
+	}
+	DestroyEngine(transformer_engine);
+	DestroyEngine(decoder_engine);
+
+	syslog(LOG_INFO, "Service shutdown.\n");
+	server_close(socket);
+
+	return RET_OK;
+}
 
 int server(char *endpoint, int use_gpu)
 {
-	return OnnxService(endpoint, (char *)"image_facegan.onnx", use_gpu);
+	return FaceGanService(endpoint, use_gpu);
 }
 
-int zoom(int socket, char *input_file)
+int facegan(int socket, char *input_file)
 {
 	int rescode;
-	IMAGE *send_image;
+	IMAGE *send_image, *resize_send_image;
 	TENSOR *send_tensor, *recv_tensor;
 
-	printf("Zooming %s ...\n", input_file);
+	printf("Face Searching %s ...\n", input_file);
 
 	send_image = image_load(input_file); check_image(send_image);
+	resize_send_image = image_zoom(send_image, 256, 256, 1); check_image(resize_send_image);
 
-	if (image_valid(send_image)) {
-		send_tensor = tensor_from_image(send_image, 0);
+	if (image_valid(resize_send_image)) {
+		send_tensor = tensor_from_image(resize_send_image, 0);
 		check_tensor(send_tensor);
 
 		recv_tensor = OnnxRPC(socket, send_tensor, IMAGE_FACEGAN_REQCODE, &rescode);
@@ -47,8 +220,9 @@ int zoom(int socket, char *input_file)
 		}
 
 		tensor_destroy(send_tensor);
-		image_destroy(send_image);
+		image_destroy(resize_send_image);
 	}
+	image_destroy(send_image);
 
 	return RET_OK;
 }
@@ -106,7 +280,7 @@ int main(int argc, char **argv)
 			return RET_ERROR;
 
 		for (i = 1; i < argc; i++)
-			zoom(socket, argv[i]);
+			facegan(socket, argv[i]);
 
 		client_close(socket);
 		return RET_OK;
